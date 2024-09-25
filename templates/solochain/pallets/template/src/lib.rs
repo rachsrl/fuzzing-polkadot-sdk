65da1836af5dd7224efdd6c9ff2f67a6ee12a1ed
Rachna Shriwas <rachna@srlabs.de>
2024-09-25 16:56:06 +0200

    added unsafe arithmetic

--- templates/solochain/pallets/template/src/lib.rs
+++ templates/solochain/pallets/template/src/lib.rs
@@ -191,7 +191,9 @@ pub mod pallet {
 				Some(old) => {
 					// Increment the value read from storage. This will cause an error in the event
 					// of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					// let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					let new = old + 1;
+
 					// Update the value in storage with the incremented result.
 					Something::<T>::put(new);
 					Ok(())
